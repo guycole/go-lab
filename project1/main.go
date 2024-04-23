692f757945da3a07faff1203571487eea1c9a033
Guy Cole <guycole@gmail.com>
2024-04-22 17:02:55 -0700

    test

--- project1/main.go
+++ project1/main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Printf("project1\n")
+
+	temp1 := []string{"a", "b", "c"}
+	fmt.Printf("temp1: %v\n", temp1)
+
+	var result string
+	for _, element := range temp1 {
+		if len(result) > 0 {
+			result += ", "
+		}
+
+		result += element
+	}
+
+	fmt.Printf("result: %v\n", result)
 }
